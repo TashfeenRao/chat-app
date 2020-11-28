3925ffc0451a92bcd478f7ec3d4c142562904cea
tashfeenrao <raotashfeen25@gmail.com>
2020-11-29 02:16:14 +0500

    read from firestore

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import firebase from "firebase";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBISpu66jDd6DLuUC5dW7ByU3yrkBlsiNk",
+  authDomain: "chat-app-8b4ed.firebaseapp.com",
+  databaseURL: "https://chat-app-8b4ed.firebaseio.com",
+  projectId: "chat-app-8b4ed",
+  storageBucket: "chat-app-8b4ed.appspot.com",
+  messagingSenderId: "84285058558",
+  appId: "1:84285058558:web:1be51772efa04ea0c224af",
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
 
 function App() {
+  const [channels, setChannels] = useState([
+    { topic: "someone please talk", id: "general" },
+  ]);
+
+  useEffect(() => {
+    const docs = [];
+    db.collection("channels").onSnapshot((snapshot) => {
+      snapshot.forEach((doc) => {
+        docs.push({ ...doc.data(), id: doc.id });
+      });
+    });
+  }, []);
   return (
     <div className="App">
       <div className="Nav">
@@ -11,17 +37,18 @@ function App() {
             src="https://placekitten.com/64/64"
           />
           <div>
-            <div>Ryan Peterson Florence</div>
+            <div>Tashfeen Rao</div>
             <div>
               <button className="text-button">log out</button>
             </div>
           </div>
         </div>
         <nav className="ChannelNav">
-          <a href="/channel/awesome"># awesome</a>
-          <a className="active" href="/channel/general">
-            # general
-          </a>
+          {channels.map((channel) => (
+            <a key={channel.id} href={`/channel/${channel.id}`}>
+              {channel.id}
+            </a>
+          ))}
         </nav>
       </div>
       <div className="Channel">
