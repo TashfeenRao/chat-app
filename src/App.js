017afdf0666351505195cb113905e7356c49ee7b
tashfeenrao <raotashfeen25@gmail.com>
2020-11-29 07:46:57 +0500

    persistent user added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Channel from "./components/Channels/Channel";
 import Nav from "./components/Nav/Nav";
 import { firebase } from "./firebase";
@@ -6,11 +6,20 @@ import { firebase } from "./firebase";
 function App() {
   const [user, setUser] = useState(null);
 
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    });
+  }, []);
+
   const handleSignIn = async () => {
     try {
       const provider = new firebase.auth.GoogleAuthProvider();
-      const result = await firebase.auth().signInWithPopup(provider);
-      setUser(result.user);
+      await firebase.auth().signInWithPopup(provider);
     } catch (error) {
       console.log(error);
     }
